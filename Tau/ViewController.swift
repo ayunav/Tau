66c18eaa06063b5b9a47d299a540ae57cdbe1e65
Ayuna NYC <ayunanyc@Ayunas-MacBook-Pro.local>
2016-11-04 22:19:47 -0400

    Add Weather struct and set up UI (labels and outlets)

--- Tau/ViewController.swift
+++ Tau/ViewController.swift
@@ -8,13 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WeatherGetterDelegate, UITextFieldDelegate {
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var weatherLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cloudCoverLabel: UILabel!
+    @IBOutlet weak var windLabel: UILabel!
+    @IBOutlet weak var rainLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var getCityWeatherButton: UIButton!
+    
+    var weather: WeatherGetter!
+    
+    // MARK: -
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let weather = WeatherGetter()
-        weather.getWeather("newyork")
+        weather = WeatherGetter(delegate: self)
+        
+        // Initialize UI
+        // -------------
+        cityLabel.text = ""
+        weatherLabel.text = ""
+        temperatureLabel.text = ""
+        cloudCoverLabel.text = ""
+        windLabel.text = ""
+        rainLabel.text = ""
+        humidityLabel.text = ""
+        cityTextField.text = ""
+        
+        cityTextField.placeholder = "Enter city name"
+        cityTextField.delegate = self
+        cityTextField.enablesReturnKeyAutomatically = true
+        
+        getCityWeatherButton.enabled = false
+        
+        //weather.getWeatherByCity("newyork")
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,6 +54,112 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - Button events
+    // ---------------------
 
+    @IBAction func getWeatherForCityButtonTapped(sender: UIButton) {
+        guard let text = cityTextField.text where !text.isEmpty else {
+            return
+        }
+        weather.getWeather(cityTextField.text!.urlEncoded)
+    }
+ 
+    // MARK: -
+    
+    // MARK: WeatherGetterDelegate methods
+    // -----------------------------------
+    
+    func didGetWeather(weather: Weather) {
+        // This method is called asynchronously, which means it won't execute in the main queue.
+        // ALl UI code needs to execute in the main queue, which is why we're wrapping the code
+        // that updates all the labels in a dispatch_async() call.
+        dispatch_async(dispatch_get_main_queue()) {
+            self.cityLabel.text = weather.city
+            self.weatherLabel.text = weather.weatherDescription
+            self.temperatureLabel.text = "\(Int(round(weather.tempCelsius)))°"
+            self.cloudCoverLabel.text = "\(weather.cloudCover)%"
+            self.windLabel.text = "\(weather.windSpeed) m/s"
+            
+            if let rain = weather.rainfallInLast3Hours {
+                self.rainLabel.text = "\(rain) mm"
+            }
+            else {
+                self.rainLabel.text = "None"
+            }
+            
+            self.humidityLabel.text = "\(weather.humidity)%"
+        }
+    }
+    
+    func didNotGetWeather(error: NSError) {
+        // This method is called asynchronously, which means it won't execute in the main queue.
+        // ALl UI code needs to execute in the main queue, which is why we're wrapping the call
+        // to showSimpleAlert(title:message:) in a dispatch_async() call.
+        dispatch_async(dispatch_get_main_queue()) {
+            self.showSimpleAlert("Can't get the weather", message: "The weather service is not responding.")
+        }
+        print("didNotGetWeather error: \(error)")
+    }
+    
+    // MARK: - UITextFieldDelegate and related methods
+    // -----------------------------------------------
+    
+    // Enable the "Get weather for the city" button
+    // if the city text field contains any text,
+    // disable it otherwise.
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        let currentText = textField.text ?? ""
+        let prospectiveText = (currentText as NSString).stringByReplacingCharactersInRange(range, withString: string)
+        getCityWeatherButton.enabled = prospectiveText.characters.count > 0
+        print("Count: \(prospectiveText.characters.count)")
+        return true
+    }
+ 
+    // Pressing the clear button on the text field (the x-in-a-circle button
+    // on the right side of the field)
+    func textFieldShouldClear(textField: UITextField) -> Bool {
+        // Even though pressing the clear button clears the text field,
+        // this line is necessary.
+        textField.text = ""
+        getCityWeatherButton.enabled = false
+        return true
+    }
+    
+    // Pressing the return button on the keyboard should be like
+    // pressing the "Get weather for the city above" button.
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        getWeatherForCityButtonTapped(getCityWeatherButton)
+        return true
+    }
+    
+    // Tapping on the view should dismiss the keyboard.
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    // MARK: - Utility methods
+    // -----------------------
+    
+    func showSimpleAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        let okAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        alert.addAction(okAction)
+        presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    
 }
+    
+extension String {
+    
+    // A handy method for %-encoding strings containing spaces and other
+    // characters that need to be converted for use in URLs.
+    
+    var urlEncoded: String {
+        return self.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLUserAllowedCharacterSet())!
+    }
+    
+}
+
 
--- Tau/WeatherGetter.swift
+++ Tau/WeatherGetter.swift
@@ -8,57 +8,89 @@
 
 import Foundation
 
+// MARK: WeatherGetterDelegate
+// ===========================
+// WeatherGetter should be used by a class or struct, and that class or struct
+// should adopt this protocol and register itself as the delegate.
+// The delegate's didGetWeather method is called if the weather data was
+// acquired from OpenWeatherMap.org and successfully converted from JSON into
+// a Swift dictionary.
+// The delegate's didNotGetWeather method is called if either:
+// - The weather was not acquired from OpenWeatherMap.org, or
+// - The received weather data could not be converted from JSON into a dictionary.
+
+protocol WeatherGetterDelegate {
+    func didGetWeather(weather: Weather)
+    func didNotGetWeather(error: NSError)
+}
+
 class WeatherGetter {
     
     private let openWeatherMapBaseURL = "http://api.openweathermap.org/data/2.5/weather"
     private let openWeatherMapAPIKey = "082f85a53bf94ae987b3e5e281a71e51"
     
-    func getWeather(city: String) {
+    private var delegate: WeatherGetterDelegate
+    
+    init(delegate: WeatherGetterDelegate) {
+        self.delegate = delegate
+    }
+    
+//    func getWeatherByCity(city: String) {
+//        let weatherRequestURL = NSURL(string: "\(openWeatherMapBaseURL)?APPID=\(openWeatherMapAPIKey)&q=\(city)")!
+//        getWeather(weatherRequestURL)
+//    }
+    
+    func getWeather(weatherRequestURL: NSURL) {
         
         let session = NSURLSession.sharedSession()
         
-        let weatherRequestURL = NSURL(string: "\(openWeatherMapBaseURL)?APPID=\(openWeatherMapAPIKey)&q=\(city)")!
-        
         let dataTask = session.dataTaskWithURL(weatherRequestURL) {
             (data: NSData?, response: NSURLResponse?, error: NSError?) in
-            if let error = error {
-                print("Error:\n\(error)")
+           
+            if let networkError = error {
+                self.delegate.didNotGetWeather(networkError)
             }
             else {
                 do {
                     // Try to convert that data into a Swift dictionary
-                    let weather = try NSJSONSerialization.JSONObjectWithData(data!, options: .MutableContainers) as! [String: AnyObject]
+                    let weatherData = try NSJSONSerialization.JSONObjectWithData(data!, options: .MutableContainers) as! [String: AnyObject]
                     
                     // If we made it to this point, we've successfully converted the
                     // JSON-formatted weather data into a Swift dictionary.
-                    // Let's print its contents to the debug console.
-                    print("Date and time: \(weather["dt"]!)")
-                    print("City: \(weather["name"]!)")
+                    // Let's now use that dictionary to initialize a Weather struct.
+                    let weather = Weather(weatherData: weatherData)
                     
-                    print("Longitude: \(weather["coord"]!["lon"]!!)")
-                    print("Latitude: \(weather["coord"]!["lat"]!!)")
-                    
-                    print("Weather ID: \(weather["weather"]![0]!["id"]!!)")
-                    print("Weather main: \(weather["weather"]![0]!["main"]!!)")
-                    print("Weather description: \(weather["weather"]![0]!["description"]!!)")
-                    print("Weather icon ID: \(weather["weather"]![0]!["icon"]!!)")
-                    
-                    print("Temperature: \(weather["main"]!["temp"]!!)")
-                    print("Humidity: \(weather["main"]!["humidity"]!!)")
-                    print("Pressure: \(weather["main"]!["pressure"]!!)")
-                    
-                    print("Cloud cover: \(weather["clouds"]!["all"]!!)")
-                    
-                    print("Wind direction: \(weather["wind"]!["deg"]!!) degrees")
-                    print("Wind speed: \(weather["wind"]!["speed"]!!)")
-                    
-                    print("Country: \(weather["sys"]!["country"]!!)")
-                    print("Sunrise: \(weather["sys"]!["sunrise"]!!)")
-                    print("Sunset: \(weather["sys"]!["sunset"]!!)")
+                    // Now that we have the Weather struct, let's notify the view controller,
+                    // which will use it to display the weather to the user.
+                    self.delegate.didGetWeather(weather)
+//                    
+//                    print("Date and time: \(weather["dt"]!)")
+//                    print("City: \(weather["name"]!)")
+//                    
+//                    print("Longitude: \(weather["coord"]!["lon"]!!)")
+//                    print("Latitude: \(weather["coord"]!["lat"]!!)")
+//                    
+//                    print("Weather ID: \(weather["weather"]![0]!["id"]!!)")
+//                    print("Weather main: \(weather["weather"]![0]!["main"]!!)")
+//                    print("Weather description: \(weather["weather"]![0]!["description"]!!)")
+//                    print("Weather icon ID: \(weather["weather"]![0]!["icon"]!!)")
+//                    
+//                    print("Temperature: \(weather["main"]!["temp"]!!)")
+//                    print("Humidity: \(weather["main"]!["humidity"]!!)")
+//                    print("Pressure: \(weather["main"]!["pressure"]!!)")
+//                    
+//                    print("Cloud cover: \(weather["clouds"]!["all"]!!)")
+//                    
+//                    print("Wind direction: \(weather["wind"]!["deg"]!!) degrees")
+//                    print("Wind speed: \(weather["wind"]!["speed"]!!)")
+//                    
+//                    print("Country: \(weather["sys"]!["country"]!!)")
+//                    print("Sunrise: \(weather["sys"]!["sunrise"]!!)")
+//                    print("Sunset: \(weather["sys"]!["sunset"]!!)")
                 }
                 catch let jsonError as NSError {
                     // An error occurred while trying to convert the data into a Swift dictionary.
-                    print("JSON error description: \(jsonError.description)")
+                    self.delegate.didNotGetWeather(jsonError)
                 }
             }
             
